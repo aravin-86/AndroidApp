6583317cd62e27b1c18c161eaa7cc4666fb0df7e
Aravinthan Narasimhan <aravin.cse@gmail.com>
2011-09-17 16:37:45 +0100

    Revision 1

--- src/com/sample/helloandroid/HelloAndroidActivity.java
+++ src/com/sample/helloandroid/HelloAndroidActivity.java
@@ -10,7 +10,7 @@ public class HelloAndroidActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TextView tv = new TextView(this);
-        tv.setText("Hello, Android World");
+        tv.setText("Hello, Android World. This is test app.");
         setContentView(tv);
     }
 }
